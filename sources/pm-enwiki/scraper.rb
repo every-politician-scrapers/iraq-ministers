dd3511af0258deec907a22c419990346e4a3d1a1
Tony Bowden <tony@tmtm.com>
2022-01-21 08:29:35 +0200

    compare enwiki Prime Ministers

---
+++ sources/pm-enwiki/scraper.rb
@@ -0,0 +1,36 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[_ img name start end].freeze
+    end
+
+    def tds
+      noko.css('td,th')
+    end
+
+    def raw_end
+      super.gsub(/\(.*\)/, '').tidy
+    end
+
+    def empty?
+      tds[2].text == tds[3].text
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
