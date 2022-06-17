ec9c9313c4f97db329346c2d247374e193ea34d0
Tony Bowden <tony@tmtm.com>
2022-06-17 14:18:18 +0300

    update to latest wpoffice

--- sources/higher-education-arwiki/scraper.rb
+++ sources/higher-education-arwiki/scraper.rb
@@ -4,41 +4,6 @@
 require 'every_politician_scraper/scraper_data'
 require 'pry'
 
-# Arabic dates
-class Arabic < WikipediaDate
-  REMAP = {
-    'حتى الأن'      => '',
-    'يناير'        => 'January',
-    'كانون الثاني' => 'January',
-    'شباط'         => 'February',
-    'فبراير'       => 'February',
-    'آذار'         => 'March',
-    'مارس'         => 'March',
-    'نيسان'        => 'April',
-    'أبريل'        => 'April',
-    'مايو'         => 'May',
-    'أيار'         => 'May',
-    'يونيو'        => 'June',
-    'حزيران'       => 'June',
-    'يوليو'        => 'July',
-    'تموز'         => 'July',
-    'أغسطس'        => 'August',
-    'آب'           => 'August',
-    'أيلول'        => 'September',
-    'سبتمبر'       => 'September',
-    'أكتوبر'       => 'October',
-    'تشرين الأول'   => 'October',
-    'نوفمبر'       => 'November',
-    'تشرين الثاني' => 'November',
-    'كانون الأول'   => 'December',
-    'ديسمبر'       => 'December',
-  }.freeze
-
-  def remap
-    super.merge(REMAP)
-  end
-end
-
 class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
   decorator UnspanAllTables
@@ -51,14 +16,6 @@ class OfficeholderList < OfficeholderListBase
   class Officeholder < OfficeholderBase
     def columns
       %w[_ name img start end].freeze
-    end
-
-    def tds
-      noko.css('td,th')
-    end
-
-    def date_class
-      Arabic
     end
   end
 end
